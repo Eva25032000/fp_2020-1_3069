32af118f0add86d2d601a98669ff9080d0a8df31
Eva <angelinaruizeva@gmail.com>
2019-10-07 10:32:11 -0500

    nueva carpetas

---
+++ TAREAS/TAREA1/tarea1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Tarea 1 que reciba 1 argumento y lo imprima en pantalla */
+
+int main(int argc, char *argv[]) {
+	printf("%s\n",argv[1]);
+	return 0;
+}
