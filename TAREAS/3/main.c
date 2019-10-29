7dc945c485327617656a1cd497ac12ed4f84dd0b
eva <angelinaruizeva@gmail.com>
2019-10-28 23:55:05 -0600

    practica11

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	double numero,numero2,operacion1,operacion2,operacion3,operacion4;
 	numero=atof(argv[1]);
@@ -11,9 +9,10 @@ int main(int argc, char *argv[]) {
 	operacion2=numero-numero2;
 	operacion3=numero*numero2;
 	operacion4=numero/numero2;
-	printf("La suma de los numeros es de:%.2f\n",operacion1);
-	printf("La diferencia de los numeros es de:%.2f\n",operacion2);
-	printf("La multiplicacion de los numeros es de:%.2f\n",operacion3);
-	printf("La division de los numeros es de:%.2f\n",operacion4);
+	printf("%.2f\n",operacion1);
+	printf("%.2f\n",operacion2);
+	printf("%.2f\n",operacion3);
+	printf("%.2f\n",operacion4);
+
 	return 0;
 }
