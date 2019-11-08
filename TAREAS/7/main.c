e6ded9fa24840e7bd40670025a651f32b38ca369
Eva <angelinaruizeva@gmail.com>
2019-11-07 19:20:10 -0600

    correcion de potencia

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
 int main(int argc,char*argv[]){
-    double operacion,base,potencia;//declaramos las variables
-    base=atof(argv[1]);//cabiamos el primer argumento a double sera la base
-    potencia=atof(argv[2]);//cambiamos el segundo argumento a double sera la potencia
-    operacion=pow(base,potencia);//operamos ambas variables
-    printf("%.2f\n",operacion);//imprimos el resultado
+    int a,operacion,base,potencia;//declaramos las variables
+    base=atoi(argv[1]);//cabiamos el primer argumento a double sera la base
+    potencia=atoi(argv[2]);//cambiamos el segundo argumento a double sera la potencia
+    operacion=1;
+	for(a=0;a<potencia;a++){//este for sirve para que la multiplicacion de la misma base se efectue segun la potencia
+    operacion=operacion*base;//vamos concatenando los valores
+	}
+	printf("%i\n",operacion);//imprimos el resultado
 	 
 	return 0;
 } 
